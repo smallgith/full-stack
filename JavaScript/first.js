6bc3eedd517ae1ea07f5f60604a3927e1f479214
smallgith <shubhambabariya.2004@gmail.com>
2025-10-17 11:10:41 +0530

    error solved

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,191 +1,99 @@
-
-
-    let btnClick = document.querySelector('#btnClick')
-    
-    btnClick.addEventListener('click',()=>{
-        
-        alert("Hello world for external js")
-    })
-
-    let lumpImg = document.getElementById('lump_img')
-    let onLump = document.getElementById('onLump')
-    let offLump = document.getElementById('offLump')
-    onLump.addEventListener('click',()=>{
-        lumpImg.src = 'https://www.w3schools.com/js/pic_bulbon.gif'
-    })
-    offLump.addEventListener('click',()=>{
-        lumpImg.src = 'https://www.w3schools.com/js/pic_bulboff.gif'
-
-        
-    })
-
-
-
-
-    // ? There are two type of value : 
-    //  ? 1.Variable
-    // ? 2.Fixed value 
-
-    // ? Javascript has multiple keyword it works like action to be performed
-
-    // ! JS Variable
-    // ? 1.let 
-    // ? 2.Const
-    
-    // ? 1.let 
-        // Todo => let can be redeclare it
-        // Todo => We may not initilizate it 
-        // ? 2.Const
-        // Todo => const can not be redeclare it 
-        // Todo => We must be initilizate it 
-
-
-    // ! identifier
-    // ?identifier which means  name you give to a variable
-
-    // let a = 10
-    // here a is identifier
-
-
-    let arr = [10,20,30,40,50,60,70,80,90]
-
-    console.log(arr[arr.length-1]) 
-
-
-    // ! There are four way to declare in js
-    /*
-       * 1.let
-       * 2.const
-       * 3.var
-       * 4.automatically
-    
-    */
-  
-    // console.log("5"+"2"+3)
-    // console.log("5"+"2"-3)
-    // console.log(5+3-"2")
-
-
-/*
-Hosting => means declare like var or function befotr initilization
-let a;
-console.log(a) without init value
-a = 10 
-*/
-hello()
-function hello(){
-    var a;
-    console.log("answer is a = ",a)
-    a = 10
-}
-
-    const person = {
-        name: "Rahul",
-        greet: function () {
-            console.log("Hello " + this.name);
-        }
-    };
-    
-    // const greetFn = person.greet;
-    // greetFn(); 
-    const greetFnBound = person.greet.bind(person);
-    greetFnBound();
-
-
-const a = 10
-
-
-
-    let pattern = document.querySelector('#pattern')
-    let patternDisplay = document.querySelector('.patternDisplay')
-    
-   
-    
-   let patterBtn = document.querySelector('.patterBtn');
-   
-   patterBtn.addEventListener('click', () => {
-       let patterns = parseInt(pattern.value)
-        let output = ""
- for(let i=1;i<=patterns;i++){
-     let sum = ""
-     for(let j=1;j<=i;j++){
-        sum += i + ""
-    }
-    output += sum + "<br>"
-    
-    
-}
-patternDisplay.innerHTML = output;
-
+// Button Click Alert
+document.querySelector('#btnClick')?.addEventListener('click', () => {
+    alert("Hello world for external js");
 });
 
+// Light Bulb Toggle
+const lumpImg = document.getElementById('lump_img');
+document.getElementById('onLump')?.addEventListener('click', () => {
+    lumpImg.src = 'https://www.w3schools.com/js/pic_bulbon.gif';
+});
+document.getElementById('offLump')?.addEventListener('click', () => {
+    lumpImg.src = 'https://www.w3schools.com/js/pic_bulboff.gif';
+});
 
+// Array Example
+const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+console.log(arr[arr.length - 1]);
 
-    // console.log("Hello-5")
-    var ab = 1
-    ab++  // ? post increment
-    console.log(ab)
-    
-    let b = 1
-    ++b  // ? pre increment
-    console.log(b) 
+// Hoisting Example
+hello();
+function hello() {
+    var a;
+    console.log("answer is a = ", a);
+    a = 10;
+}
 
+// Bind Example
+const person = {
+    name: "Rahul",
+    greet() {
+        console.log("Hello " + this.name);
+    }
+};
+const greetFnBound = person.greet.bind(person);
+greetFnBound();
 
-    let text="";
+// Pattern Generator (Secure version - no innerHTML)
+const pattern = document.querySelector('#pattern');
+const patternDisplay = document.querySelector('.patternDisplay');
+document.querySelector('.patterBtn')?.addEventListener('click', () => {
+    const patterns = parseInt(pattern.value);
+    patternDisplay.innerHTML = ""; // Clear old content
+    for (let i = 1; i <= patterns; i++) {
+        const line = document.createElement("div");
+        line.textContent = String(i).repeat(i);
+        patternDisplay.appendChild(line);
+    }
+});
+
+// Increment Test
+var ab = 1;
+console.log(ab++); // post-increment
+let b = 1;
+console.log(++b); // pre-increment
+
+// While Loop
+let output = "";
 let i = 1;
-
 while (i < 10) {
-  text += i;
-  i++;
+    output += i;
+    i++;
 }
-console.log(text)
+console.log(output);
 
+// Cars Array
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+cars.forEach(car => console.log(car));
 
+// String charAt & at
+let msg = "Hello world";
+console.log(msg.charAt(2));
+console.log(msg.at(2));
 
-    const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// Concat Example
+let first = "Hello";
+let second = "World";
+console.log(first + " " + second);
 
-let text1 = "";
-for (let i = 0; i < cars.length; i++) {
-  console.log(cars[i])
+// Split Example
+let sample = "Hello";
+const myArr = sample.split("");
+myArr.forEach(char => console.log(char));
+
+// Word Split Loop
+let sentence = "hello world is the best for everyone";
+sentence.split(" ").forEach(word => console.log(word));
+
+// Infinity Example
+let aa = 10 / 0;
+console.log(aa);
+
+// Function Example
+function fun(name, age) {
+    console.log(name, age);
 }
+fun("Shubham", 22);
 
-
-
-    let text0 = "Hello world"
-    console.log(text.charAt(2))  // It is used for extract character from string
-
-    let text2 = "Hello world"
-    console.log(text.at(2)) // Both are same
-    
-
-    let text01 = "Hello"
-    let text11 = "World"
-    console.log(text.concat(" ",text1))
-
-
-    let text123 = "Hello";
-const myArr = text.split("");
-
-text = "";
-for (let i = 0; i < myArr.length; i++) {
- console.log(myArr[i])
-
-    let string = "hello world is the best for everyone"
-    let arr = string.split(" ")
-    for(i=0;i<arr.length; i++){
-        console.log(arr[i])
-    }
-}
-
-    let aa = 10/0
-    console.log(a)
-
-
-    function fun(name,age){
-        console.log(name,age)
-    }
-    fun("Shubham",22)
-
-
-    const d = new Date(2004)
-    console.log(d)
+// Date Example
+console.log(new Date(2004));
